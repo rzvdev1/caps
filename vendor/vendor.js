8ad9eaf7b19975823fcb6e9a070c1b31cc4edb7f
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-08-12 21:20:21 -0700

    :construction: Queue added

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -4,39 +4,6 @@ const client = io('ws://localhost:3000/caps');
 
 const events = require('../eventPool.js');
 
-// var Chance = require('chance');
-
-// // Instantiate Chance so it can be used
-// let chance = new Chance();
-
-// let customer = `${chance.first()} ${chance.last()}`;
-// let address = `${chance.address()} ${chance.city()} ${chance.state()} ${chance.zip()}`;
-// let orderId = chance.guid();
-// let time = new Date().toISOString();
-// let store = chance.company();
-
-// events.on('pickup', pickup);
-// events.on('in-transit', inTransit);
-// events.on('delivered', delivered);
-
-// function pickup() {
-//   let payload = {
-//     store: store,
-//     orderId: orderId,
-//     customer: customer,
-//     address: address,
-//   };
-//   console.log('EVENT', { event: 'pickup', time: time, payload: payload });
-// }
-
-// function inTransit() {
-//   console.log('EVENT', { event: 'in-transit', time: time, payload: orderId });
-// }
-
-// function delivered() {
-//   console.log('EVENT', { event: 'delivered', time: time, payload: orderId });
-//   console.log(`Thank you for your order ${customer}!`);
-// }
 var Chance = require('chance');
 
 // Instantiate Chance so it can be used
@@ -45,6 +12,25 @@ let chance = new Chance();
 let customer = `${chance.first()} ${chance.last()}`;
 let address = `${chance.address()} ${chance.city()} ${chance.state()} ${chance.zip()}`;
 let orderId = chance.guid();
+
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+  // the driver queue will hold driver sockets
+  // the package queue will hold payloads
+  enqueue(item) {
+    this.queue.unshift(item);
+  }
+
+  dequeue() {
+    return this.queue.pop();
+  }
+
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+}
 
 const payload = {
   customerId: customer,
@@ -64,4 +50,4 @@ client.on(events.pickedUp, (payload) =>
 client.on(events.delivered, (payload) =>
   console.log(payload.message, payload.orderId)
 );
-module.exports = { client };
+module.exports = { client, Queue };
